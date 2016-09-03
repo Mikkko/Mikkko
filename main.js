3d633c21dddd6a216e4f5ba3913e5009566a0cf0
Vovk Oleg <vovkoleg1987@gmail.com>
2016-09-03 11:42:40 +0300

    idi nahuy

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+console.log('lolggr');
+
+
+var obj = {
+    login: 'user1',
+    password: 'qwerty',
+    email: 'KOSTYA.PAVLOV@gmail.com'
+};
+console.log(obj);
+
+obj = JSON.stringify(obj);
+
+console.log(obj);
